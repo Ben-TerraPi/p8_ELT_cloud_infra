3e2c61af4c2bae111e032067027f55f041b6b300
Benoît Dourdet <benoit.dourdet@gmail.com>
2026-05-21 15:40:52 +0200

    [fix] colonne date & time

--- tests/int_underground_data_no_duplicates.sql
+++ tests/int_underground_data_no_duplicates.sql
@@ -1,6 +1,6 @@
 -- {{ config(severity = "error") }}
 -- Vérifier qu'on n'a pas de doublons station_id/date_time en intermediate
-SELECT station_id, date_time, COUNT(*) as cnt
+SELECT station_id, date, time, COUNT(*) as cnt
 FROM {{ ref('int_underground_data') }}
-GROUP BY station_id, date_time
+GROUP BY station_id, date, time
 HAVING COUNT(*) > 1
